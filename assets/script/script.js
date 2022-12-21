caae8f87d8431a5ad35d70a24bf0a5271a6a840c
sanjybekitacademy <sanzybekzamirbekov@gmail.com>
2022-12-21 20:40:41 +0600

    massage

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+document.querySelector('div').classList.add('container')
+document.querySelector('div').classList.add('cards')
+document.querySelector('div').classList.add('card')
+
+
+
+function moneyConverter (valnum) {
+    document.getElementById('outputsum').innerHTML=valnum*86;
+}
+
+
